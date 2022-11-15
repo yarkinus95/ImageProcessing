0c0833e2c92b4a2be2b427ebf219afcf6d7940be
yarkinus95 <yarkinyoru95@gmail.com>
2022-11-15 10:28:40 +0300

    Update

--- thresholdtest.py
+++ thresholdtest.py
@@ -4,9 +4,9 @@ import numpy as np
 def nothing(x):
     pass
 
-img3=cv2.imread("robert.png")
-    img3 = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    img3 = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    img3=cv2.imread("robert.png")
+    img3 = cv2.cvtColor(img3, cv2.COLOR_BGR2RGB)
+    img3 = cv2.cvtColor(img3, cv2.COLOR_RGB2GRAY)
     kernelX=np.array([[1,1,1],[0,0,0],[-1,-1,-1]])
     kernelY=np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
     prwX=cv2.filter2D(img3,-1,kernelX)
